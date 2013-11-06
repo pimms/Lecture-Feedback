ddc4cc5f224298d60e501168585f8e77c35b6a2c
Joakim Stien <joakim.stien@gmail.com>
2013-11-06 22:54:54 +0100

    Integrated some of TobbenTM's code and made things work SOMEWHAT ok.

--- src/chipmunk/unlimited/feedback/TimeEditParser.java
+++ src/chipmunk/unlimited/feedback/TimeEditParser.java
@@ -1,19 +1,8 @@
 package chipmunk.unlimited.feedback;
 
-import java.io.ByteArrayInputStream;
-import java.io.StringReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import android.util.Log;
 
@@ -22,7 +11,11 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 /**
  * @class TimeEditParser
  * Parser of TimeEdit HTML. The parsing is done using XPath and
- * DOM traversal. 
+ * DOM traversal.
+ * 
+ * This class is heavily based on TobbenTM's implementation
+ * of a TimeEdit parser. The TimeEdit HTML is full of invalid
+ * HTML, causing the w3c.dom-parser to fail. 
  */
 public class TimeEditParser extends AsyncHttpResponseHandler {
 	/* The constants defines how the parser interprets
@@ -44,7 +37,11 @@ public class TimeEditParser extends AsyncHttpResponseHandler {
 	@Override 
 	public void onSuccess(String response) {
 		if (mContentType == CONTENT_TIMETABLE) {
-			parseTimeTable(response);
+			String[][] res = parseTimeTable(response);
+			Log.d("ITEM", res.length + " items.");
+			for (String[] arr : res) {
+				Log.d("ITEM ("+arr.length+")", "[0]="+arr[0] + "[1]=" + arr[1] + "[2]=" + arr[2] + "[3]=" + arr[3] + "[4]=" + arr[4]);
+			}
 		}
 	}
 	
@@ -54,94 +51,163 @@ public class TimeEditParser extends AsyncHttpResponseHandler {
 	}
 	
 	
-	/**
-	 * Parse and retrieve lecture data from a time-table webpage.
-	 * 
-	 * @param rawHtml
-	 * HTML expected to make up a TimeEdit time-table page.
-	 */
-	private void parseTimeTable(String rawHtml) {
-		try {
-			rawHtml = commentHtmlLines(rawHtml, 3, 10);
-			Log.d("HTML", rawHtml);
-			
-			DocumentBuilder b = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document doc = b.parse(new ByteArrayInputStream(rawHtml.getBytes()));
-			
-			/* Retrieve the <tbody> element containing the 
-			 * desired data. 3 <tbody> elements exists, but only one
-			 * contain usedful data. The XPATH expression should 
-			 * filter out the other two. 
-			 */
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			NodeList nodes = (NodeList)xpath.evaluate(TIMETABLE_XPATH_ROOT, 
-								doc.getDocumentElement(), XPathConstants.NODESET);
-			
-			if (nodes.getLength() != 1) {
-				/* We're likely without internet connection, or the
-				 * webpage structure has changed.  
-				 */
-				displayErrorDialog("Invalid DOM structure received. Time-table expected.");
-			} else {
-				/* Parse the <tbody> element */
-				Element tbody = (Element)nodes.item(0);
-				parseTimeTableDOM(tbody);
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			displayErrorDialog(ex.getMessage());
-		} 
-	}
-	
-	private void parseTimeTableDOM(Element tbody) {
-		String date = "";
-		
-		// Discard the first child 
-		Node node = tbody.getFirstChild();
-		
-		while (node != null ) {
-			String attrClass = getNodeAttribute(node, "class");
-			
-			if (attrClass == null) {
-				/* The element indicates a new day */
-				Node dateNode = node.getFirstChild().getNextSibling();
-				date = dateNode.getTextContent();
-			} else if (attrClass == "clickable2") {
-				/* The first two nodes are irrelevant */
-				Node tdNode = node.getFirstChild().getNextSibling().getNextSibling();
-				
-				String course = tdNode.getTextContent();
-				tdNode = tdNode.getNextSibling();
-				
-				String room = tdNode.getTextContent();
-				tdNode = tdNode.getNextSibling();
-				
-				String lecturer = tdNode.getTextContent();
-				
-				Log.d("DEBUGZ", course + lecturer + room);
-			}
-			
-			node = node.getNextSibling();
-		}
-	}
-	
-	
-	private String getNodeAttribute(Node node, String attribute) {
-		Node attr = node.getAttributes().getNamedItem(attribute);
-		
-		if (attr != null) {
-			return attr.getTextContent();
-		}
-		
-		return null;
-	}
-	
-	/**
-	 * Display an error dialog
-	 */
-	private void displayErrorDialog(String message) {
-		Log.e("Until further ado", message);
-	}
+	public static String[][] parseTimeTable(String html){
+        String[] split1 = html
+                .replaceAll("<span class=\"tesprite tesprite-floatright tesprite-new\"></span>", "")
+                .split("/table");
+        String[] split2 = split1[0].split("changeDateLink headline leftRounded t .*\">");
+
+        String[][] results = new String[split2.length-1][6];
+        ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
+        String currentdate = "";
+
+        for(int i = 1; i < split2.length -1; i++){
+            currentdate = split2[i].split("</td>")[0].replaceAll(" Today", "");
+            String[] split3 = split2[i].split("</tr>");
+            master.add(dateEntry(currentdate));
+            for(int j = 1; j < split3.length -1; j++){
+                ArrayList<String> inner = new ArrayList<String>();
+                String[] data = tagData(split3[j]);
+                inner.add(currentdate);
+                for(String s : data){
+                    inner.add(s);
+                }
+                master.add(inner);
+            }
+        }
+        return dimensionalPortal(master);
+    }
+
+    public static String[][] parseTimeTable(String html, boolean room){
+        String[] split1 = html
+                .replaceAll("<span class=\"tesprite tesprite-floatright tesprite-new\"></span>", "")
+                .split("/table");
+        String[] split2 = split1[0].split("changeDateLink headline leftRounded t .*\">");
+        String startTime, endTime;
+
+        String[][] results = new String[split2.length-1][6];
+        ArrayList<ArrayList<String>> master = new ArrayList<ArrayList<String>>();
+        String currentdate = "";
+
+        for(int i = 1; i < split2.length -1; i++){
+            currentdate = split2[i].split("</td>")[0].replaceAll(" Today", "");
+            String[] split3 = split2[i].split("</tr>");
+            master.add(dateEntry(currentdate));
+            endTime = "0800";
+            for(int j = 1; j < split3.length -1; j++){
+                ArrayList<String> inner = new ArrayList<String>();
+                String[] data = tagData(split3[j]);
+                if(room){
+                    startTime = data[0].split("\n")[0].replace(":", "");
+                    if(
+                            Integer.parseInt(startTime) >= 815 &&
+                            Integer.parseInt(startTime) > Integer.parseInt(endTime)+16 &&
+                            Integer.parseInt(endTime) < 1600 ){
+                        master.add(clearEntry(currentdate, timeString(endTime, startTime)));
+                        Log.d("TIME", "Added clear entry: " + timeString(endTime, startTime));
+                    }
+                    endTime = data[0].split("\n")[2].replace(":", "");
+                }
+                inner.add(currentdate);
+                for(String s : data){
+                    inner.add(s);
+                }
+                Log.d("TIME", "Lecture time: \t\t" + data[1]);
+                master.add(inner);
+            }
+        }
+        return dimensionalPortal(master);
+    }
+
+    public static String[][] search(String html, String term){
+        //Log.d("PARSING", "Starting parser");
+        List<String> id = new ArrayList<String>();
+        List<String> name = new ArrayList<String>();
+        List<String> ids = new ArrayList<String>();
+        List<String> names = new ArrayList<String>();
+
+        //The reason for splitting at data-id="-1" is that there is no valuable data
+        // after it, and it would just cause problems.
+        //Still not bug-free though, and should be replaced with a better regex-based
+        // solution.
+        String[] split = html.split("data-id=\"-1\"");
+
+        ids.addAll(Arrays.asList(split[0].split("data-id=\"")));
+        ids.remove(0);
+        names.addAll(Arrays.asList(split[0].split("data-name=\"")));
+        names.remove(0);
+
+
+        for (String s : ids){
+            id.add(s.split("\"")[0]);
+        }
+        for (String s : names){
+            name.add(s.split("\"")[0]);
+        }
+
+        String[][] result = new String[id.size()][2];
+        Log.d("HIG.SEARCH.ARRAY", name.toString() + id.toString());
+
+        //Join the two arrays into one 2D array
+        for(int i=0; i < id.size(); i++){
+            result[i][0] = id.get(i);
+            result[i][1] = name.get(i);
+        }
+        return result;
+    }
+
+    private static String[] tagData(String html){
+        String[] split = html.replaceAll("(?m:</tr>|^[\\s]*)", "").split("[\\s]*<[^<]+?>");
+        List<String> tagdata = new ArrayList<String>();
+        //Log.d("TAG DATA \t", Arrays.toString(split));
+        for(int i = 4; i < split.length -4; i += 1){
+            if(i == 4)tagdata.add(split[i].replace(" ", "\n"));
+            if(i == 6 || i == 8 || i == 10 || i == 12) tagdata.add(split[i]);
+        }
+        String[] tagdata2 = new String[tagdata.size()];
+        tagdata2 = tagdata.toArray(tagdata2);
+        return tagdata2;
+    }
+
+    private static String[][] dimensionalPortal(ArrayList<ArrayList<String>> arraylist){
+        final int size = arraylist.size();
+        String[][] sarr = new String[size][];
+        for(int i = 0; i < size; i++) {
+            ArrayList<String> innerlist = arraylist.get(i);
+            final int innerSize = innerlist.size();
+            sarr[i] = new String[innerSize];
+            for(int j = 0; j < innerSize; j++) {
+                sarr[i][j] = innerlist.get(j);
+            }
+        }
+        return sarr;
+    }
+
+    private static ArrayList<String> dateEntry(String date){
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add(date);
+        arr.add("");
+        arr.add("HIGREADER.newDate");
+        arr.add("");
+        arr.add("");
+        arr.add("");
+        return arr;
+    }
+
+    private static ArrayList<String> clearEntry(String date, String time){
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add(date);
+        arr.add(time);
+        arr.add("HIGREADER.clear");
+        arr.add("");
+        arr.add("");
+        arr.add("");
+        return arr;
+    }
+
+    private static String timeString(String start, String end){
+        return start.replaceFirst("([0-9]{2})", "$1:") + " - " + end.replaceFirst("([0-9]{2})", "$1:");
+    }
 }
 
 
