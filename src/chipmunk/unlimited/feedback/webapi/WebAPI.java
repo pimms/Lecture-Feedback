35d281c2ea42eaf96caf526ed738b56d015fb335
pimms <joakim.stien@gmail.com>
2013-11-13 12:21:04 +0100

    Added WebAPI subpackage

---
+++ src/chipmunk/unlimited/feedback/webapi/WebAPI.java
@@ -0,0 +1,11 @@
+package chipmunk.unlimited.feedback.webapi;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+/**
+ * @class WebAPI
+ * Java interface for communicating with the webAPI
+ */
+public class WebAPI extends AsyncHttpResponseHandler {
+	public static final String API_URL = "192.168.2.63";
+}
