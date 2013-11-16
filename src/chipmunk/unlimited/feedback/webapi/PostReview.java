90e07846f6847342e676769fc0e017ac7e7e84db
pimms <joakim.stien@gmail.com>
2013-11-16 17:07:13 +0100

    fixed stupid bug

--- src/chipmunk/unlimited/feedback/webapi/PostReview.java
+++ src/chipmunk/unlimited/feedback/webapi/PostReview.java
@@ -1,11 +1,16 @@
 package chipmunk.unlimited.feedback.webapi;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.apache.http.Header;
+
 import chipmunk.unlimited.feedback.LectureReviewItem;
 
 public class PostReview extends WebAPICall {
 	public interface PostReviewCallback {
 		public void onPostReviewSuccess();
-		public void onPostReviewFailure();
+		public void onPostReviewFailure(String errorMessage);
 	}
 	
 	private PostReviewCallback mCallback;
@@ -17,5 +22,37 @@ public class PostReview extends WebAPICall {
 	
 	public void apiCall(String baseUrl, LectureReviewItem review) {
 		
+	}
+	
+	
+	@Override
+	public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+		String response;
+		
+		try {
+			response = new String(responseBody, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			mCallback.onPostReviewFailure("Returned content is not UTF-8 encoded");
+			return;
+		}
+		
+	}
+	
+	@Override 
+	public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+		String bodyString;
+		String errorMessage = "HTTP Error with code " + statusCode;
+		
+		try {
+			if (responseBody != null) {
+				bodyString = new String(responseBody, "UTF-8");
+				errorMessage += " (" + bodyString + ")";
+			}
+		} catch (UnsupportedEncodingException e) {
+			mCallback.onPostReviewFailure("HTTP error with code " + statusCode);
+			return;
+		}
+		
+		mCallback.onPostReviewFailure(errorMessage);
 	}
 }
