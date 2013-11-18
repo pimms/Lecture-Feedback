4ffe622f12b75a4c34ed331e74c1cba568b39fa1
Joakim Stien <joakim.stien@gmail.com>
2013-11-18 18:22:23 +0100

    Starting activity when pressed on stats view

--- src/chipmunk/unlimited/feedback/stats/StatisticsFragment.java
+++ src/chipmunk/unlimited/feedback/stats/StatisticsFragment.java
@@ -2,18 +2,23 @@ package chipmunk.unlimited.feedback.stats;
 
 import chipmunk.unlimited.feedback.MainActivityFragmentInterface;
 import chipmunk.unlimited.feedback.R;
+import chipmunk.unlimited.feedback.revfeed.FeedActivity;
+import chipmunk.unlimited.feedback.subscription.SubscriptionItem;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class StatisticsFragment extends Fragment
-            implements MainActivityFragmentInterface {
+            implements MainActivityFragmentInterface,
+                       AdapterView.OnItemClickListener {
     private StatisticsAdapter mAdapter;
-
+    
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class StatisticsFragment extends Fragment
         mAdapter = new StatisticsAdapter(getActivity());
         ListView listView = (ListView)rootView.findViewById(R.id.stats_list_view);
         listView.setAdapter(mAdapter);
+        listView.setOnItemClickListener(this);
 
 		return rootView;
 	}
@@ -29,5 +35,17 @@ public class StatisticsFragment extends Fragment
     @Override
     public void refreshContents() {
         mAdapter.reloadSubscriptions();
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        SubscriptionItem subItem = (SubscriptionItem)mAdapter.getItem(i);
+
+        if (subItem != null) {
+            Intent intent = new Intent(getActivity(), FeedActivity.class);
+            intent.putExtra(FeedActivity.PARAM_SINGLE_COURSE, "hei");
+            startActivity(intent);
+        }
     }
 }
