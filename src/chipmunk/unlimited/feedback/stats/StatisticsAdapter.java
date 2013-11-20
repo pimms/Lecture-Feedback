ceba8f62c5e11cf68f90f955f8cb440fc9c8f3f1
Joakim Stien <joakim.stien@gmail.com>
2013-11-20 14:53:34 +0100

    The course votes are now displayed in the stats view

--- src/chipmunk/unlimited/feedback/stats/StatisticsAdapter.java
+++ src/chipmunk/unlimited/feedback/stats/StatisticsAdapter.java
@@ -3,6 +3,7 @@ package chipmunk.unlimited.feedback.stats;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 import chipmunk.unlimited.feedback.R;
 import chipmunk.unlimited.feedback.database.SubscriptionDatabase;
 import chipmunk.unlimited.feedback.subscription.SubscriptionItem;
+import chipmunk.unlimited.feedback.webapi.WebAPI;
+import chipmunk.unlimited.feedback.webapi.WebAPI.*;
 
 
 /**
@@ -20,17 +23,22 @@ import chipmunk.unlimited.feedback.subscription.SubscriptionItem;
  * The adapter retrieves the votes for the courses, and updates
  * the list view when the votes arrive.
  */
-public class StatisticsAdapter extends BaseAdapter {
+public class StatisticsAdapter extends BaseAdapter
+                               implements GetCourseVotesCallback {
+    private static final String TAG = "StatisticsAdapter";
 	
 	private List<SubscriptionItem> mSubscriptions;
+    private List<CourseVote> mCourseVotes;
 	private LayoutInflater mInflater;
 	private Context mContext;
+
 	
 	public StatisticsAdapter(Context context) {
 		mContext = context;
-		mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
-	
+
+
 	@Override
 	public int getCount() {
 		if (mSubscriptions == null) {
@@ -57,13 +65,41 @@ public class StatisticsAdapter extends BaseAdapter {
 		}
 		
 		TextView tvName = (TextView)convertView.findViewById(R.id.course_text_view_name);
-		
+		TextView tvPos  = (TextView)convertView.findViewById(R.id.course_text_view_positive);
+        TextView tvNeg  = (TextView)convertView.findViewById(R.id.course_text_view_negative);
+
 		SubscriptionItem sub = (SubscriptionItem)getItem(position);
 		tvName.setText(sub.getName());
+
+        // Set the vote texts
+        CourseVote vote = getCourseVote(sub.getHigCode());
+        if (vote != null) {
+            tvPos.setText("" + vote.getPositiveVoteCount());
+            tvNeg.setText("" + vote.getNegativeVoteCount());
+        } else {
+            tvPos.setText("?");
+            tvNeg.setText("?");
+        }
 		
 		return convertView;
 	}
-	
+
+    public CourseVote getCourseVote(String higCode) {
+        CourseVote vote = null;
+
+        if (mCourseVotes != null) {
+            // Attempt to find the vote object
+            for (CourseVote cv : mCourseVotes) {
+                if (cv.getCourseCode().equals(higCode)) {
+                    vote = cv;
+                    break;
+                }
+            }
+        }
+
+        return vote;
+    }
+
 	
 	public void reloadSubscriptions() {
 		loadSubscriptions();
@@ -73,7 +109,23 @@ public class StatisticsAdapter extends BaseAdapter {
 	private void loadSubscriptions() {
 		SubscriptionDatabase db = new SubscriptionDatabase(mContext);
 		mSubscriptions = db.getSubscriptionList();
+
+        WebAPI webApi = new WebAPI();
+        webApi.getCourseVotes(this, mSubscriptions);
 	}
+
+
+    @Override
+    public void onGetCourseVotesSuccess(List<CourseVote> items) {
+        mCourseVotes = items;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onGetCourseVotesFailure(String errorMessage) {
+        mCourseVotes = null;
+        Log.e(TAG, "Failed to get course votes: " + errorMessage);
+    }
 }
 
 
