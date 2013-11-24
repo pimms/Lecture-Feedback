6326399bb16dec2549db7587b9650d4c764228ba
Joakim Stien <joakim.stien@gmail.com>
2013-11-24 18:17:27 +0100

    BAD JOAKIM, BAD. Code like that is not allowed

--- src/chipmunk/unlimited/feedback/webapi/PostReview.java
+++ src/chipmunk/unlimited/feedback/webapi/PostReview.java
@@ -63,7 +63,7 @@ class PostReview extends WebAPICall {
 			if (json != null && json.getString("status").equals("ok")) {
 				mCallback.onPostReviewSuccess();
 			} else {
-				throw new Exception("bad JSON status");
+				mCallback.onPostReviewFailure("Post failed: api returned status bad");
 			}
 		} catch (Exception e) {
 			mCallback.onPostReviewFailure("Failed to parse json: " + e.getMessage());
