19f2b8b6e68663d8338e94b7a08a0776918c336f
Joakim Stien <joakim.stien@gmail.com>
2013-12-02 11:04:06 +0100

    Placeholder

--- src/chipmunk/unlimited/feedback/highscore/TopCourseAdapter.java
+++ src/chipmunk/unlimited/feedback/highscore/TopCourseAdapter.java
@@ -1,14 +1,27 @@
 package chipmunk.unlimited.feedback.highscore;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import chipmunk.unlimited.feedback.R;
+
 public class TopCourseAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+
+
+    public TopCourseAdapter(Context context) {
+        mContext = context;
+        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
 
     @Override
     public int getCount() {
-        return 0;
+        return 3;
     }
 
     @Override
@@ -23,6 +36,12 @@ public class TopCourseAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if (view == null || view.getId() != R.layout.list_item_course) {
+            mInflater.inflate(R.layout.list_item_course, null);
+        }
+
+
+
+        return view;
     }
 }
