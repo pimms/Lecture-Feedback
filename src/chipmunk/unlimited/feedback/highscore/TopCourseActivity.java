c4f42ce05fadc3b0148e602c1b786c25c7c1ce33
Joakim Stien <joakim.stien@gmail.com>
2013-12-02 10:55:28 +0100

    Added an empty activity for highscore courses

---
+++ src/chipmunk/unlimited/feedback/highscore/TopCourseActivity.java
@@ -0,0 +1,11 @@
+package chipmunk.unlimited.feedback.highscore;
+
+import android.app.ListActivity;
+
+/**
+ * Activity displaying the highest rated courses in descending
+ * order.
+ */
+public class TopCourseActivity extends ListActivity {
+
+}
