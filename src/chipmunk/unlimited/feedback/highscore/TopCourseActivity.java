472e37083af815f3432d2dd9687cfaaa74021049
Joakim Stien <joakim.stien@gmail.com>
2013-12-02 11:00:09 +0100

    Init

--- src/chipmunk/unlimited/feedback/highscore/TopCourseActivity.java
+++ src/chipmunk/unlimited/feedback/highscore/TopCourseActivity.java
@@ -1,11 +1,24 @@
 package chipmunk.unlimited.feedback.highscore;
 
 import android.app.ListActivity;
+import android.os.Bundle;
+
+import chipmunk.unlimited.feedback.R;
 
 /**
  * Activity displaying the highest rated courses in descending
  * order.
  */
 public class TopCourseActivity extends ListActivity {
+    TopCourseAdapter mAdapter;
 
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_listview);
+
+        mAdapter = new TopCourseAdapter();
+        getListView().setAdapter(mAdapter);
+    }
 }
