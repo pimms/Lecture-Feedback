ec096a640908e3169fd89a9df2e938e6e503dfce
Knut Remi Løvli <mail@knutremi.com>
2013-12-06 15:56:56 +0100

    Wrote some test data

--- tests/src/chipmunk/unlimited/feedback/test/LectureReviewItemTest.java
+++ tests/src/chipmunk/unlimited/feedback/test/LectureReviewItemTest.java
@@ -8,8 +8,37 @@ import android.test.AndroidTestCase;
 
 public class LectureReviewItemTest extends AndroidTestCase {
 
-	private LectureReviewItem lectureReviewItem1; // LectureItem lecture, boolean[] ratings, String comment, int id, Date reviewDate
-	private LectureReviewItem lectureReviewItem2; // String date, String time, String name, String higCode, String room, String lecturer, boolean[] ratings, String comment, int id, Date reviewDate
+	// Our testable units. One for each constructor.
+	private LectureReviewItem lectureReviewItem1;
+	private LectureReviewItem lectureReviewItem2;
+	
+	// Test data for the first constructor.
+	private LectureItem lectureItem; // The first constructor takes an object as a parameter.
+	private String date1 = "2013-12-06";
+	private String time1 = "14:32 - 14:33";
+	private String name1 = "Mobil utviklingsprosjekt";
+	private String higCode1 = "IMT3672";
+	private String room1 = "D101";
+	private String lecturer1 = "Nowostawski";
+	private boolean[] ratings1 = {false, false, true, true, false}; // Notice the corresponding ratingString1 below.
+	private String comment1 = "Test comment 1";
+	private int id1 = -1; // TODO: Determine whether we should test this for an existing ID as well. Or generate one.
+	private Date reviewDate1 = null; // TODO: Should we generate the date here?
+	
+	// The stringified array we expect back from getRatingString().
+	private String ratingString1 = "0.0.1.1.0";
+	
+	// Test data for the second constructor.
+	private String date2 = "2013-12-06";
+	private String time2 = "15:03 - 15:04";
+	private String name2 = "Mobil Systemutvikling";
+	private String higCode2 = "IMT3662";
+	private String room2 = "B211";
+	private String lecturer2 = "Nowostawski";
+	private boolean[] ratings2 = {true, false, false, true, true};
+	private String comment2 = "Test comment 2";
+	private int id2 = -1;
+	private Date reviewDate2 = null;
 	
 	/**
 	 * Required to run the test.
@@ -32,67 +61,8 @@ public class LectureReviewItemTest extends AndroidTestCase {
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		super.setUp();
-		
-		/**
-		 * Create a LectureReviewItem based on a LectureItem.
-		 * 
-		 * @param lecture
-		 * LectureItem to be passed to the super constructor.
-		 * 
-		 * @param ratings
-		 * Array containing five values, where false is
-		 * negative and true is positive.
-		 * 
-		 * @param comment
-		 * The comment of the user. Should not be null, but an empty
-		 * string on no comment.
-		 * 
-		 * @param id
-		 * The unique ID of the review, received from the web API. 
-		 * For submit-instances, -1 should be passed.
-		 * 
-		 * @param reviewDate
-		 * The date the comment was made. Null is a suitable value
-		 * for submit-instances.
-		 */
-		
-		
-		
-		/**
-		 * @param date
-		 * See LectureItem.
-		 * 
-		 * @param time
-		 * See LectureItem.
-		 * 
-		 * @param name
-		 * See LectureItem.
-		 * 
-		 * @param higCode
-		 * See LectureItem
-		 * 
-		 * @param room
-		 * See LectureItem.
-		 * 
-		 * @param lecturer
-		 * See LectureItem.
-		 * 
-		 * @param ratings
-		 * Array containing five values, where false is
-		 * negative and true is positive.
-		 * 
-		 * @param comment
-		 * The comment of the user. Should not be null, but an empty
-		 * string on no comment.
-		 * 
-		 * @param id
-		 * The unique ID of the review, received from the web API. 
-		 * For submit-instances, -1 should be passed.
-		 * 
-		 * @param reviewDate
-		 * The date the comment was made. Null is a suitable value
-		 * for submit-instances.
-		 */
+//		lectureReviewItem1 = new LectureReviewItem(new LectureItem(date, time, name, higCode, room, lecturer), ratings, comment, id, reviewDate);
+//		lectureReviewItem2 = new LectureReviewItem(date, time, name, higCode, room, lecturer, ratings, comment, id, reviewDate);
 		
 	}
 
