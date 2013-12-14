d588fd8b2c45d1fc01c8190041ac962d692867b1
Joakim Stien <joakim.stien@gmail.com>
2013-12-14 11:20:28 +0100

    Added check for network connectivity

--- src/chipmunk/unlimited/feedback/webapi/HttpClient.java
+++ src/chipmunk/unlimited/feedback/webapi/HttpClient.java
@@ -1,12 +1,25 @@
 package chipmunk.unlimited.feedback.webapi;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestHandle;
 import com.loopj.android.http.ResponseHandlerInterface;
 
 
-class HttpClient extends AsyncHttpClient {
+public class HttpClient extends AsyncHttpClient {
+    public static boolean isInternetAvailable(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return  activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
+
     @Override
     public RequestHandle get(String url, ResponseHandlerInterface callback) {
         return super.get(url, callback);
